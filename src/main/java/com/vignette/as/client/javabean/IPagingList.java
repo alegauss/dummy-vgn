1a42a92d6608623a44b8a34fd99f8521d3ff211b
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-06-09 15:40:47 -0300

    - IPagingList as Interface

--- src/main/java/com/vignette/as/client/javabean/IPagingList.java
+++ src/main/java/com/vignette/as/client/javabean/IPagingList.java
@@ -5,18 +5,11 @@ import com.vignette.as.client.exception.ApplicationException;
 import java.util.Iterator;
 import java.util.List;
 
-public class IPagingList {
+public interface IPagingList {
 
-	public List<Object> asList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public List<Object> asList();
 
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	public Iterator pageIterator(int pageSize) throws ApplicationException {
-		return null;
-	}
+	public int size();
+
+	public Iterator pageIterator(int pageSize) throws ApplicationException;
 }
