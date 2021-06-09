fe4f91c9bb96d50d86fb17528eb5937eb597588b
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2021-06-09 14:54:29 -0300

    AsLocale getAsLocale() throws ApplicationException

--- src/main/java/com/vignette/as/client/common/ref/AsLocaleRef.java
+++ src/main/java/com/vignette/as/client/common/ref/AsLocaleRef.java
@@ -1,10 +1,11 @@
 package com.vignette.as.client.common.ref;
 
+import com.vignette.as.client.exception.ApplicationException;
 import com.vignette.as.client.javabean.AsLocale;
 
 public class AsLocaleRef {
 
-    public AsLocale getAsLocale() {
+    public AsLocale getAsLocale() throws ApplicationException {
         return null;
     }
 }
